eb77b084355cd0c9d6e25c53c0d0797cffa25577
elbwrlimx <elberlima44@gmail.com>
2025-03-19 19:10:38 -0300

    subindo projeto

---
+++ react02/projeto01/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='app'>
+    <header className='App-header'>
+      <h1>Lista de Tarefas</h1>
+    </header>
+      
+    </div>
+  )
+}
+
+export default App
